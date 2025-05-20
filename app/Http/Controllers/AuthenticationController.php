e8775b6d527b8b9403e4588a874c2e7b3684425f
Menna-Baligh <mennabaligh55520@yahoo.com>
2025-05-20 20:18:47 +0300

    Add AuthenticationController with registration, login, and logout functionality

---
+++ app/Http/Controllers/AuthenticationController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthenticationController extends Controller
+{
+    public function register(){
+        return view('auth.register');
+    }
+    public function store(Request $request){
+        $request->validate([
+            'name' => ['required' , 'min:3'],
+            'email' => ['required' , 'email'],
+            'password' => ['required' , 'min:5' , 'confirmed'] ,
+        ]);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+        auth()->login($user);
+        return to_route('posts.index');
+
+    }
+    public function login(){
+        return view('auth.login');
+    }
+    public function authenticate(Request $request){
+        $credentials = $request->validate([
+            'email' => ['required' , 'email'],
+            'password' => ['required' , 'min:5']
+        ]);
+        if(auth()->attempt($credentials)){
+            $request->session()->regenerate();
+            return to_route('posts.index');
+        }
+        return back()->withErrors([
+            'email' => 'Invalid credentials'
+        ]);
+
+    }
+    public function logout(){
+        auth()->logout();
+        request()->session()->invalidate();
+        request()->session()->regenerateToken();
+        return to_route('login');
+    }
+}
